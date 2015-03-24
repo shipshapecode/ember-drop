3f31ccb56af086c3b880d03e0294307da24708ba
Robert Wagner <rwwagner@Roberts-MacBook-Pro.local>
2015-03-24 12:28:17 -0400

    Got content and events working

--- addon/components/ember-drop.js
+++ addon/components/ember-drop.js
@@ -5,10 +5,37 @@ export default Ember.Component.extend({
   didInsertElement: function() {
     Ember.run.scheduleOnce('afterRender', this, function() {
       var drop;
-
+      var content = document.createElement('div');
+      content = $(content);
+      if (this.get('content') && Array.isArray(this.get('content'))) {
+        this.get('content').forEach(function(element) {
+          var elementType = null;
+          if (element.type === 'button') {
+            elementType = 'button';
+          }
+          else if (element.type === 'text') {
+            elementType = 'div';
+          }
+          if (elementType) {
+            var elementToAppend = document.createElement(elementType);
+            elementToAppend = $(elementToAppend);
+            if (element.text) {
+              elementToAppend.text(element.text);
+            }
+            if (element.events) {
+              if (element.events.click && typeof element.events.click === 'function') {
+                elementToAppend.click(element.events.click);
+              }
+            }
+            content.append(elementToAppend);
+          }
+        });
+      }
+      content.addClass('content');
+      content = content[0];
       drop = new Drop({
         classes: this.get('classes'),
-        content: this.get('content'),
+        content: content,
         openOn: this.get('openOn'),
         position: this.get('position'),
         target: document.querySelector(this.get('targetSelector'))
--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,7 +2,38 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   setupController: function(controller, model) {
-    var testHtml = '<button onclick="alert(\'hi\')">Test</button>';
-    controller.set('testHtml', testHtml);
+    var hoverContent = [
+      {
+        type: 'text',
+        text: 'You can specify openOn=\'hover\''
+      }
+    ];
+    controller.set('hoverContent', hoverContent);
+
+    var clickContent = [
+      {
+        type: 'text',
+        text: 'Or openOn=\'click\''
+      }
+    ];
+    controller.set('clickContent', clickContent);
+
+    var testContent = [
+      {
+        type: 'button',
+        text: 'Change name',
+        events: {
+          click: function() {
+            this.set('name', 'Thomas Jefferson');
+          }.bind(controller)
+        }
+      },
+      {
+        type: 'text',
+        text: 'Click to change the name in the greeting'
+      }
+    ];
+
+    controller.set('testContent', testContent);
   }
 });
