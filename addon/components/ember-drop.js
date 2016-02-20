0da2044515f6b5101efc13beaf5966eac858e76e
Robert Wagner <rwwagner@vt.edu>
2016-02-20 17:00:41 -0500

    Remove jquery dependency

--- addon/components/ember-drop.js
+++ addon/components/ember-drop.js
@@ -1,6 +1,5 @@
 /* globals Drop */
 import Ember from 'ember';
-import $ from 'jquery';
 
 export default Ember.Component.extend({
   didInsertElement(){
@@ -11,38 +10,33 @@ export default Ember.Component.extend({
   }),
   initialize() {
     Ember.run.scheduleOnce('afterRender', this, function() {
-      let drop;
-      let content = document.createElement('div');
-      content = $(content);
+      const content = document.createElement('div');
       if (this.get('content') && Array.isArray(this.get('content'))) {
-        this.get('content').forEach(function(element) {
+        this.get('content').forEach((element) => {
           if (element.type) {
             let elementToAppend = document.createElement(element.type);
-            elementToAppend = $(elementToAppend);
             if (element.classes) {
-              elementToAppend.addClass(element.classes);
+              elementToAppend.classList.add(element.classes);
             }
             if (element.text) {
-              elementToAppend.text(element.text);
+              elementToAppend.textContent = element.text;
             }
             if (element.events) {
               if (element.events.click && typeof element.events.click === 'function') {
-                elementToAppend.click(element.events.click);
+                elementToAppend.onclick = element.events.click;
               }
             }
             let containerDiv = document.createElement('div');
-            containerDiv = $(containerDiv);
-            containerDiv.append(elementToAppend);
-            content.append(containerDiv);
+            containerDiv.appendChild(elementToAppend);
+            content.appendChild(containerDiv);
           }
         });
       }
-      content.addClass('content');
-      content = content[0];
-      drop = new Drop({
+      content.classList.add('content');
+      const drop = new Drop({
         classes: this.get('classes') || '',
-        constrainToScrollParent: this.get('constrainToScrollParent') === false ? false : true,
-        constrainToWindow: this.get('constrainToWindow') === false ? false : true,
+        constrainToScrollParent: this.get('constrainToScrollParent') !== false,
+        constrainToWindow: this.get('constrainToWindow') !== false,
         content: content,
         openOn: this.get('openOn') || 'click',
         position: this.get('position') || 'bottom left',
@@ -54,7 +48,7 @@ export default Ember.Component.extend({
     });
   },
 
-  willDestroyElement: function() {
+  willDestroyElement() {
     if (this.get('drop')) {
       this.get('drop').destroy();
     }
