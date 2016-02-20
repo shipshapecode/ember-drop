e22b9c4e8171eea251e38985a312aabff97518fb
Robert Wagner <rwwagner@vt.edu>
2016-02-20 16:47:41 -0500

    Get app working with new ember

--- addon/components/ember-drop.js
+++ addon/components/ember-drop.js
@@ -3,15 +3,21 @@ import Ember from 'ember';
 import $ from 'jquery';
 
 export default Ember.Component.extend({
-  initialize: function() {
+  didInsertElement(){
+    this.initialize();
+  },
+  contentChanged: Ember.observer('content', function() {
+    this.initialize();
+  }),
+  initialize() {
     Ember.run.scheduleOnce('afterRender', this, function() {
-      var drop;
-      var content = document.createElement('div');
+      let drop;
+      let content = document.createElement('div');
       content = $(content);
       if (this.get('content') && Array.isArray(this.get('content'))) {
         this.get('content').forEach(function(element) {
           if (element.type) {
-            var elementToAppend = document.createElement(element.type);
+            let elementToAppend = document.createElement(element.type);
             elementToAppend = $(elementToAppend);
             if (element.classes) {
               elementToAppend.addClass(element.classes);
@@ -24,7 +30,7 @@ export default Ember.Component.extend({
                 elementToAppend.click(element.events.click);
               }
             }
-            var containerDiv = document.createElement('div');
+            let containerDiv = document.createElement('div');
             containerDiv = $(containerDiv);
             containerDiv.append(elementToAppend);
             content.append(containerDiv);
@@ -46,7 +52,8 @@ export default Ember.Component.extend({
       });
       this.set('drop', drop);
     });
-  }.on('didInsertElement').observes('content'),
+  },
+
   willDestroyElement: function() {
     if (this.get('drop')) {
       this.get('drop').destroy();
