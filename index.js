1b6f36433832ae4404d6152db32d44d4541dc495
Robert Wagner <rwwagner90@gmail.com>
2016-10-28 08:00:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 module.exports = {
   name: 'ember-drop',
   included: function(app) {
-    this._super.included(app);
+    this._super.included.apply(this, arguments);
     this.app.import(app.bowerDirectory + '/tether/dist/js/tether.js');
     this.app.import(app.bowerDirectory + '/tether-drop/dist/css/drop-theme-arrows-bounce-dark.css');
     this.app.import(app.bowerDirectory + '/tether-drop/dist/js/drop.min.js');
