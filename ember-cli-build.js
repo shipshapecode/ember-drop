08dfb25559c707d839e49a37bf8099e24c51b14d
Robert Wagner <rwwagner90@gmail.com>
2017-06-05 11:38:59 -0400

    Use ember-prism

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,17 +5,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   const app = new EmberAddon(defaults, {
-    nodeAssets: {
-      prismjs() {
-        return {
-          import: [
-            'prism.js',
-            'components/prism-bash.js',
-            'components/prism-handlebars.js'
-          ],
-        };
-      }
-    }
   });
 
   return app.toTree();
